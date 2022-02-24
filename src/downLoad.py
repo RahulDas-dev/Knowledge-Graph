33a2a68da40ba3e5750b79ebca120b9612577d05
Rahul_Das <r.das699@gmail.com>
2022-02-25 00:22:42 +0530

    model.py added

--- src/downLoad.py
+++ src/downLoad.py
@@ -1,15 +1,15 @@
-"""Modules Defines functional blocks for Downloading various text dataset."""
+"""Modules defines functional blocks for Downloading various text dataset."""
 
 import concurrent.futures
 
 import pandas as pd
-from tqdm import tqdm
 import wikipediaapi  # pip install wikipedia-api
+
+from tqdm import tqdm
 
 
 def wikipedia_scrape(topic, links_count=10, verbose=True):
-    """
-    Download Pages related to given topic from Wikipedia .
+    """Download Pages related to given topic from Wikipedia.
 
         Parameters:
             topic(string): Topic Name to search for.
@@ -17,16 +17,15 @@ def wikipedia_scrape(topic, links_count=10, verbose=True):
             verbose(boolean): Display Progress bar while downloading.
 
         Returns:
-            pandas dataframe with columns 
+            pandas dataframe with columns
                 topic: topic,
                 content: Page Content,
                 url: Source URL/Link,
                 categories: As categories by wikipedia
     """
-    
     wiki_api = wikipediaapi.Wikipedia(
         language="en", extract_format=wikipediaapi.ExtractFormat.WIKI
-    ) 
+    )
 
     def donwload_reference_link(link):
         """Downloads individual links."""
@@ -39,14 +38,14 @@ def wikipedia_scrape(topic, links_count=10, verbose=True):
                     "url": page.fullurl,
                     "categories": list(page.categories.keys()),
                 }
-            else: 
-                return None    
+            else:
+                return None
         except BaseException:
             return None
 
-    #Extracting Main Page related to given topic.
-    main_page = wiki_api.page(topic) 
-    if  not main_page.exists():
+    # Extracting Main Page related to given topic.
+    main_page = wiki_api.page(topic)
+    if not main_page.exists():
         print(f"Wikipedia does not have a page on topic {topic}.")
         return None
 
@@ -57,31 +56,41 @@ def wikipedia_scrape(topic, links_count=10, verbose=True):
             "url": main_page.fullurl,
             "categories": list(main_page.categories.keys()),
         }
-    ]  
+    ]
 
-    #listing down the reference links.
-    page_links = list(main_page.links.keys()) 
-    print(f'Refenece Links count {len(page_links)}') if verbose else None
-    if  len(page_links) > links_count:
+    # listing down the reference links.
+    page_links = list(main_page.links.keys())
+    print(f"Refenece Links count {len(page_links)}") if verbose else None
+    if len(page_links) > links_count:
         page_links = page_links[:links_count]
 
-    #setting up Progress-bar.    
+    # setting up Progress-bar.
     progress = (
-        tqdm(desc="Collecting Data", unit="", total=len(page_links)) if verbose else None
-    ) 
-    
-    #concurrently downloading pages from refrence link 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: 
-        future_link = {executor.submit(donwload_reference_link, link): link for link in page_links}
+        tqdm(desc="Collecting Data", unit="", total=len(page_links))
+        if verbose
+        else None
+    )
+
+    # concurrently downloading pages from reference link
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        future_link = {
+            executor.submit(donwload_reference_link, link): link for link in page_links
+        }
         for future in concurrent.futures.as_completed(future_link):
             data = future.result()
             page_collection.append(data) if data else None
             progress.update(1) if verbose else None
     progress.close() if verbose else None
     dataframe = pd.DataFrame(page_collection)
-    dataframe["categories"] = dataframe.categories.apply(lambda x: [item.replace("Category:","") for item in x])
+    dataframe["categories"] = dataframe.categories.apply(
+        lambda x: [item.replace("Category:", "") for item in x]
+    )
     dataframe["topic"] = topic
     tqdm.pandas(desc="sentence counting:")
-    dataframe['sentence_count'] = dataframe['content'].progress_apply(lambda x: len(x.split('.')))
-    dataframe.sort_values(by=['sentence_count'],ascending=False,inplace=True,ignore_index=True)
+    dataframe["sentence_count"] = dataframe["content"].progress_apply(
+        lambda x: len(x.split("."))
+    )
+    dataframe.sort_values(
+        by=["sentence_count"], ascending=False, inplace=True, ignore_index=True
+    )
     return dataframe
--- src/model.py
+++ src/model.py
@@ -1,37 +1,172 @@
-    import pandas as pd
-    import re
-    import spacy
-    import neuralcoref
-    import networkx as nx
-    import matplotlib.pyplot as plt
+import pickle
+import re
+from datetime import datetime
+from functools import lru_cache
 
+import neuralcoref
+import pandas as pd
+import spacy
 
-    nlp = spacy.load('en_core_web_lg')
-    neuralcoref.add_to_pipe(nlp)
+# import networkx as nx
+# import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 class Model(object):
+    __nlp = spacy.load("en_core_web_lg")
+    neuralcoref.add_to_pipe(__nlp)
 
-    def __init__(self, coreference=False):
-        self.coreference = coreference 
-        pass
+    def __init__(self, entity_pairs, coreference=False):
+        self.__coreference = coreference
+        self.__unwanted_tokens = (
+            "PRON",  # pronouns
+            "PART",  # particle
+            "DET",  # determiner
+            "SCONJ",  # subordinating conjunction
+            "PUNCT",  # punctuation
+            "SYM",  # symbol
+            "X",  # other
+        )
+        self.__entity_pairs = entity_pairs if entity_pairs else []
 
-    def __extract_entity(self, input_doc):
-        pass
+    def __preprocess_doc(self, input_doc):
+        # replace multiple newlines with period
+        preprocessed_doc = re.sub(r"\n+", ".", input_doc)
+        # remove reference numbers
+        preprocessed_doc = re.sub(r"\[\d+\]", " ", preprocessed_doc)
+        preprocessed_doc = Model.__nlp(preprocessed_doc)
+        if self.__coreference:
+            preprocessed_doc = Model.__nlp(preprocessed_doc._.coref_resolved)
+        return preprocessed_doc
 
-    def __preprocess_doc(self,input_doc):
-        input_doc = re.sub(r'\n+', '.', input_doc)  # replace multiple newlines with period
-        input_doc = re.sub(r'\[\d+\]', ' ', input_doc)  # remove reference numbers
-        input_doc = nlp(input_doc)
-        if self.coreference:
-            input_doc = nlp(input_doc._.coref_resolved) 
-        return input_doc     
+    @lru_cache(maxsize=25)
+    def __refine_entity(self, entity, sentense):
+        ent_type = entity.ent_type_
+        if ent_type == "":
+            ent_type = "NOUN_CHUNK"
+            entity = " ".join(
+                str(t.text)
+                for t in Model.__nlp(str(entity))
+                if t.pos_ not in self.__unwanted_tokens and t.is_stop is False
+            )
+            return entity, ent_type
 
-    def fit(self,input_doc):
+        if ent_type in ("NOMINAL", "CARDINAL", "ORDINAL") & str(entity).find(" ") == -1:
+            refined = ""
+            for i in range(len(sentense) - entity.i):
+                if entity.nbor(i).pos_ not in ("VERB", "PUNCT"):
+                    refined += " " + str(entity.nbor(i))
+                else:
+                    entity = refined.strip()
+                    break
+            return entity, ent_type
+        else:
+            return entity, ent_type
+
+    def fit(self, input_doc):
         processed_doc = self.__preprocess_doc(input_doc)
         # split text into sentences
         sentences = [sent.string.strip() for sent in processed_doc.sents]
-        pass
+        entity_pairs = []
+        progress = tqdm(desc="Processing Sentences", unit="", total=len(sentences))
+        for sentence in sentences:
+            sentence = Model.__nlp(sentence)
+
+            spans = list(sentence.ents) + list(sentence.noun_chunks)  # collect nodes
+            spans = spacy.util.filter_spans(spans)
+            with sentence.retokenize() as retokenizer:
+                [
+                    retokenizer.merge(
+                        span, attrs={"tag": span.root.tag, "dep": span.root.dep}
+                    )
+                    for span in spans
+                ]
+
+            dependency = [token.dep_ for token in sentence]
+            # limit our example to simple sentences with one subject and object
+            if (dependency.count("obj") + dependency.count("dobj")) != 1:
+                continue
+            if (dependency.count("subj") + dependency.count("nsubj")) != 1:
+                continue
+
+            for token in sentence:
+
+                if token.dep_ not in ("obj", "dobj"):
+                    continue
+
+                subject = [w for w in token.head.lefts if w.dep_ in ("subj", "nsubj")]
+
+                if not subject:
+                    continue
+
+                subject = subject[0]
+
+                relation = [w for w in token.ancestors if w.dep_ == "ROOT"]
+
+                if relation:
+                    relation = relation[0]
+                    if relation.nbor(1).pos_ in ("ADP", "PART"):
+                        relation = " ".join((str(relation), str(relation.nbor(1))))
+                else:
+                    relation = "unknown"
+
+                subject, subject_type = self.__refine_entity(subject, sentence)
+                token, object_type = self.__refine_entity(token, sentence)
+                entity_pairs.append(
+                    [
+                        str(subject),
+                        str(relation),
+                        str(token),
+                        str(subject_type),
+                        str(object_type),
+                    ]
+                )
+            progress.update(1)
+        entity_pairs = list(
+            filter(lambda x: not any(str(item) == "" for item in x), entity_pairs)
+        )
+        self.__entity_pairs.extend(entity_pairs)
+
+    def knowlwdge_graph(self, out_format="list"):
+        if len(self.__entity_pairs) == 0:
+            raise Exception("Run the method model.fit() first")
+        if out_format == "list":
+            return self.__entity_pairs
+        elif out_format == "DataFrame":
+            return pd.DataFrame(
+                self.__entity_pairs,
+                columns=[
+                    "subject",
+                    "relation",
+                    "object",
+                    "subject_type",
+                    "object_type",
+                ],
+            )
+        else:
+            raise ValueError(f"argument format ={format} is not valid")
+
+    def save(self, file_name=""):
+        if len(self.__entity_pairs) == 0:
+            raise Exception("Run the method model.fit() first")
+        if file_name == "":
+            file_name = "Model_{}_{}.pkl".format(
+                len(self.__entity_pairs), datetime.today().strftime("%d/%m/%Y_%H:%M:%S")
+            )
+        else:
+            file_name = re.sub(r".", "_", file_name)
+            file_name = "{}.pkl".format(file_name)
+        with open(file_name, "wb") as handle:
+            pickle.dump(self.__entity_pairs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    @classmethod
+    def load(cls, file_name):
+        entitypairs = []
+        with open(file_name, "rb") as handle:
+            entitypairs = pickle.load(handle)
+        if len(entitypairs) == 0:
+            raise Exception(f"file {file_name} contains No Data")
+        return Model(coreference=False, entity_pairs=entitypairs)
 
     def visulize(self):
         pass
